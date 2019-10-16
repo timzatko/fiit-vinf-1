dfc73df8d7da29411c62cd562320735de0b57fa5
Timotej Zaťko <timi.zatko@gmail.com>
2019-10-16 10:56:24 +0200

    add item component

--- app/src/app/item/item.component.ts
+++ app/src/app/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Item} from "./item";
 
 @Component({
   selector: 'app-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
+
+  @Input() item: Item;
 
   constructor() { }
 
--- app/src/app/material/material.module.ts
+++ app/src/app/material/material.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import { MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   imports: [
     CommonModule,
     MatInputModule,
     MatIconModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   exports: [
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
   ],
 })
 export class MaterialModule { }
